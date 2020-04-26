54165a228e35ae8218a36f169de1b2d1ed49f824
W1ncenty <okonek.wojciech@gmail.com>
2020-04-26 18:13:54 +0200

    add jquery scrolling

---
+++ js/scroll.js
@@ -0,0 +1,14 @@
+const animationTime = 500;
+const sectionsToScrollTo = ['third', 'fourth', 'sixth', 'eighth'];
+
+$(document).ready(function (){
+    sectionsToScrollTo.forEach(sectionName => {
+
+        $(`#scroll-${sectionName}`).click(() => {
+            $([document.documentElement, document.body]).animate({
+                scrollTop: $(`#${sectionName}`).offset().top
+            }, animationTime);
+        });
+
+    });
+});
